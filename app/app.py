be234e9984c99cb5f623e619d7f71d26f04ac71a
Yanelis Serrano <44768783+ytaylor-usal@users.noreply.github.com>
2018-12-17 12:44:31 +0100

    -Update port to run the application with docker

--- app/app.py
+++ app/app.py
@@ -1,15 +1,8 @@
 import json
-
-from flask import Flask, jsonify
-
 import pandas as pd
+from flask import Flask, jsonify
 from pathlib import Path
-
-import json
-
 from iteration_utilities import flatten
-
-
 
 data_path = Path(__file__).resolve().parent.parent / 'data'
 static_path = Path(__file__).resolve().parent / 'static' / 'dist'
@@ -19,12 +12,9 @@ print('static path: ' + str(static_path))
 
 app = Flask(__name__, static_folder=static_path)
 
-
-
 @app.route("/")
 def index():
 	return app.send_static_file('index.html')
 
-
 if __name__ == "__main__":
-	app.run(host="localhost", port=5000, debug=True)
+	app.run(host="0.0.0.0", port=80)
